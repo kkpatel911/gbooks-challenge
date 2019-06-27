02530f6e07a2de6a4467263b35cfb437997d217c
kkpatel911 <kkpatel911@gmail.com>
2019-06-27 01:07:57 -0700

    added header component

---
+++ react-/src/components/Header/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class header extends Component {
+  render() {
+    return (
+      <header className="showCase">
+        <div className="show-top">
+          <img src={require("../../images/logo.png")} alt="logo" />
+        </div>
+        <div className="show-content">
+          <h1>Welcome to our</h1>
+          <p>Google Book Store</p>
+        </div>
+      </header>
+    );
+  }
+}
